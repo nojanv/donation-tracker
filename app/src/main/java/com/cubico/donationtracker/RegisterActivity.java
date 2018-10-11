aef3ae5bce40ad5960f2aba059401268bb689126
skee21 <31735461+skee21@users.noreply.github.com>
2018-10-11 12:08:51 -0400

    Made new error messages

--- app/src/main/java/com/cubico/donationtracker/RegisterActivity.java
+++ app/src/main/java/com/cubico/donationtracker/RegisterActivity.java
@@ -111,7 +111,7 @@ public class RegisterActivity extends AppCompatActivity{
 
         // Check for a valid password, if the user entered one.
         if (TextUtils.isEmpty(password)) {
-            mPasswordView.setError(getString(R.string.error_invalid_password));
+            mPasswordView.setError(getString(R.string.error_field_required));
             focusView = mPasswordView;
             cancel = true;
         } else if (!isPasswordValid(password)) {
