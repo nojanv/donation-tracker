85aa63b32803159071531633d4fec7211f99f2ef
Nouzhan Vakili <nojanv@gmail.com>
2018-10-12 14:28:11 -0400

    Added input validation. App also resets current firebase user upon going to WelcomeActivity.java. Also checks for emails already in use.

--- app/src/main/java/com/cubico/donationtracker/RegisterActivity.java
+++ app/src/main/java/com/cubico/donationtracker/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -18,6 +19,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -114,63 +116,132 @@ public class RegisterActivity extends AppCompatActivity{
         }
     }
 
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+    private boolean isEmailValid(String email) {
+        Model model = Model.getInstance();
+        return email.contains("@") && !model.checkEmail(email);
+    }
+
+    View focusView;
+
     /**
      * Creates the new user account with email and password.
 
      */
     public void attemptRegistration() {
 
+//        Log.d("CURR_USER", mAuth.getCurrentUser().getEmail());
+//        if(mAuth.getCurrentUser() != null) {
+//            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+//        }
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+        mConfrimPasswordView.setError(null);
+        mNameView.setError(null);
+
         name = mNameView.getText().toString();
         email = mEmailView.getText().toString();
         password = mPasswordView.getText().toString();
         cPassword = mConfrimPasswordView.getText().toString();
 
+        boolean cancel = false;
+        focusView = null;
+
+        if (TextUtils.isEmpty(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        } else if (!isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_password_short));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid matching password, if the user entered one.
+        if (TextUtils.isEmpty(cPassword) || !password.equals(cPassword)) {
+            mConfrimPasswordView.setError(getString(R.string.error_unmatched_passwords));
+            focusView = mConfrimPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        //Check for valid name.
+        if (TextUtils.isEmpty(name)) {
+            mNameView.setError(getString(R.string.error_field_required));
+            focusView = mNameView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
 
 
-        Log.d("TAG", email);
-        Log.d("TAG", password);
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()) {
-                            // store the additional fields in firebase as well
-                            User newUser = new User(name, email, accountType);
-                            FirebaseDatabase.getInstance().getReference("Users")
-                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-                                    .setValue(newUser).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()) {
-                                        Toast.makeText(RegisterActivity.this, getString(R.string.already_user), Toast.LENGTH_LONG).show();
-                                        finish();
-                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                                    } else {
-                                        Toast.makeText(RegisterActivity.this, getString(R.string.error_invalid_email), Toast.LENGTH_LONG).show();
+            Log.d("TAG", email);
+            Log.d("TAG", password);
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // store the additional fields in firebase as well
+                                User newUser = new User(name, email, accountType);
+                                FirebaseDatabase.getInstance().getReference("Users")
+                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(newUser).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Toast.makeText(RegisterActivity.this, getString(R.string.registration_success), Toast.LENGTH_LONG).show();
+                                            finish();
+                                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                        } else {
+                                            Toast.makeText(RegisterActivity.this, getString(R.string.firebase_inner_error), Toast.LENGTH_LONG).show();
+                                        }
                                     }
+                                });
+                            } else if (!task.isSuccessful()){
+                                if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                    mEmailView.setError(getString(R.string.error_email_exists));
+                                    focusView = mEmailView;
+                                    focusView.requestFocus();
                                 }
-                            });
-                        } else {
-                            Log.w("TAG", "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(RegisterActivity.this, getString(R.string.error_unmatched_passwords), Toast.LENGTH_LONG).show();
+//                                Log.w("TAG", "createUserWithEmail:failure", task.getException());
+//                                Toast.makeText(RegisterActivity.this, getString(R.string.firebase_outer_error), Toast.LENGTH_LONG).show();
 
+                            }
                         }
-                    }
-                });
-    }
-
-    /**
-     * Adds the new user's information to the database.
-     * @param newUser The instance of the new user.
-     */
-    public void addUserToDatabase(User newUser) {
-        FirebaseUser userID = FirebaseAuth.getInstance().getCurrentUser();
-        if (userID != null) {
-            DatabaseReference childRef = userRef.child(userID.getUid());
-            childRef.setValue(newUser);
-
+                    });
         }
     }
+
+//    /**
+//     * Adds the new user's information to the database.
+//     * @param newUser The instance of the new user.
+//     */
+//    public void addUserToDatabase(User newUser) {
+//        FirebaseUser userID = FirebaseAuth.getInstance().getCurrentUser();
+//        if (userID != null) {
+//            DatabaseReference childRef = userRef.child(userID.getUid());
+//            childRef.setValue(newUser);
+//
+//        }
+//    }
 
 
 }
